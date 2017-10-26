566492388766e8e2237e09281b552ef1800b5ab1
ttran9 <toddtran9@gmail.com>
2017-10-26 14:06:23 -0700

    Videos 291 and 292. Adding in Bootstrapping Vendor data and modifying the constructor as necessary to use the VendorRepository.

--- src/main/java/guru/springfamework/bootstrap/Bootstrap.java
+++ src/main/java/guru/springfamework/bootstrap/Bootstrap.java
@@ -2,8 +2,10 @@ package guru.springfamework.bootstrap;
 
 import guru.springfamework.domain.Category;
 import guru.springfamework.domain.Customer;
+import guru.springfamework.domain.Vendor;
 import guru.springfamework.repositories.CategoryRepository;
 import guru.springfamework.repositories.CustomerRepository;
+import guru.springfamework.repositories.VendorRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -13,16 +15,19 @@ public class Bootstrap implements CommandLineRunner {
 
     private CategoryRepository categoryRepository;
     private CustomerRepository customerRepository;
+    private VendorRepository vendorRepository;
 
-    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository) {
+    public Bootstrap(CategoryRepository categoryRepository, CustomerRepository customerRepository, VendorRepository vendorRepository) {
         this.categoryRepository = categoryRepository;
         this.customerRepository = customerRepository;
+        this.vendorRepository = vendorRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
         loadCategories();
         loadCustomers();
+        loadVendors();
     }
 
     private void loadCategories() {
@@ -67,5 +72,20 @@ public class Bootstrap implements CommandLineRunner {
         customerRepository.save(customer2);
 
         System.out.println("Customers loaded = " + customerRepository.count());
+    }
+
+    private void loadVendors() {
+        Vendor vendor = new Vendor();
+        vendor.setId(1L);
+        vendor.setName("Franks Fresh Fruits from France Ltd.");
+
+        Vendor vendor2 = new Vendor();
+        vendor2.setId(2L);
+        vendor2.setName("Hormel Foods Corporation");
+
+        vendorRepository.save(vendor);
+        vendorRepository.save(vendor2);
+
+        System.out.println("Vendors loaded = " + vendorRepository.count());
     }
 }
